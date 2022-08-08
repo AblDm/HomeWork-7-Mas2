91b5f555c3cac21df69f55e92d5cd4a06aca5009
Dmabl <sdfghj@dg.jnb>
2022-08-08 19:43:25 +0300

    Промежуточный коммит, 4 задание пока что в работе

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        String arrString = Arrays.toString(arr);
+        System.out.println(arr);
+        return arr;
+
+
+        //
+
+
+
     }
+    public static void main(String[] args) {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+
+
+        //task 1
+        System.out.println("\nЗадание 1");
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        System.out.print("Сумма трат за месяц составила " + sum + " рублей");
+
+        //task 2
+        System.out.println(" \n\nЗадание 2");
+        int minArr = 200_000;
+        for (int j : arr) {
+            if (j < minArr) {
+                minArr = j;
+            }
+        }
+        System.out.print("Минимальные затраты " + minArr + " рублей");
+
+        //task 3
+        System.out.println("\n\nЗадание 3");
+
+        float sum1 = 0;
+        for (int i : arr) {
+            sum1 += i;
+        }
+        float vSum = sum1 / arr.length;
+        System.out.print(vSum);
+
+
+        //task 4
+        System.out.println("\n\nЗадание 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        char synbol = 0;
+        for (int i = 0; i < reverseFullName.length/2; i++) {
+            synbol = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 -i] = synbol;}
+//
+
+        System.out.println(synbol);
+
+
+
+
+    }
+
+
 }
